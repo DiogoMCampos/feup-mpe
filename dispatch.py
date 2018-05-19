69fafe846ad5a38e25843d2e816a2f6aa2770fee
Edgar Passos <edgarlpassos@gmail.com>
2018-05-19 17:28:23 +0100

    Adding flights to terminal queues

--- dispatch.py
+++ dispatch.py
@@ -2,12 +2,12 @@
 """ dispatch.py - Dispatch the flights read from a file input as an argument according to ERD """
 
 import sys
-import queue
+from collections import deque
 
 
 def get_first_available_time(terminal):
     """ return the conveyor that is available first """
-    return terminal.index(min(terminal))
+    return terminal.index(min(terminal['conveyors']))
 
 def read_flights(file):
     """ read the flights and place them in an array """
@@ -31,32 +31,38 @@ def read_flights(file):
         flight['baggage'] = line[6]
         flight['plane'] = line[7]
 
-        print(flight)
-
+        flights_array.append(flight)
 
     return flights_array
 
 
 
-TERMINALS = []
+TERMINALS = [{} for i in range(0, 9)]
 
 # set up conveyors in terminal 1 to 5
 for i in range(1, 6):
-    TERMINALS[i] = [0, 0, 0]
+    TERMINALS[i]['conveyors'] = [0, 0, 0]
 
 # set up the rest of the terminals
-TERMINALS[7] = [0, 0, 0]
-TERMINALS[8] = [0, 0]
+TERMINALS[7]['conveyors'] = [0, 0, 0]
+TERMINALS[8]['conveyors'] = [0, 0]
 
 for terminal in TERMINALS:
-    terminal['flight_queue'] = queue.Queue()
+    terminal['flight_queue'] = deque([])
 
 INPUT_FILE = open(sys.argv[1], "r")
 FLIGHTS = read_flights(INPUT_FILE)
 
 for flight in FLIGHTS:
-    TERMINALS[flight['terminal']]['flight_queue'].put(flight)
+    if flight['terminal'] == '-':
+        continue
 
-print(TERMINALS)
+    flight_terminal = int(flight['terminal'])
+    TERMINALS[flight_terminal]['flight_queue'].append(flight)
+
+for terminal in TERMINALS:
+    print(terminal)
+    for item in terminal['flight_queue']:
+        print(item)
 
 INPUT_FILE.close()
