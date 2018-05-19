bb9a57c7b70fabd4bd0d158ccc86352715da49da
Edgar Passos <edgarlpassos@gmail.com>
2018-05-19 17:15:35 +0100

    Starting work on dispatching rules

--- dispatch.py
+++ dispatch.py
@@ -2,6 +2,12 @@
 """ dispatch.py - Dispatch the flights read from a file input as an argument according to ERD """
 
 import sys
+import queue
+
+
+def get_first_available_time(terminal):
+    """ return the conveyor that is available first """
+    return terminal.index(min(terminal))
 
 def read_flights(file):
     """ read the flights and place them in an array """
@@ -32,9 +38,25 @@ def read_flights(file):
 
 
 
+TERMINALS = []
+
+# set up conveyors in terminal 1 to 5
+for i in range(1, 6):
+    TERMINALS[i] = [0, 0, 0]
+
+# set up the rest of the terminals
+TERMINALS[7] = [0, 0, 0]
+TERMINALS[8] = [0, 0]
+
+for terminal in TERMINALS:
+    terminal['flight_queue'] = queue.Queue()
 
 INPUT_FILE = open(sys.argv[1], "r")
-
 FLIGHTS = read_flights(INPUT_FILE)
+
+for flight in FLIGHTS:
+    TERMINALS[flight['terminal']]['flight_queue'].put(flight)
+
+print(TERMINALS)
 
 INPUT_FILE.close()
